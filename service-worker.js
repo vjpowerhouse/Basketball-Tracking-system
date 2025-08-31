d2bfc5a2742d009f93f6f3e63ab065463d1b8652
vjpowerhouse <vjkumar@gmail.com>
2025-08-31 16:45:47 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "basketball-tracker-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
